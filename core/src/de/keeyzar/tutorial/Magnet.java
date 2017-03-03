7b20c2f40bf840a6b6e49f007f006d7818cc6132
Kevin Kekule <k.kekule@web.de>
2017-03-03 20:36:57 +0100

    init commit

---
+++ core/src/de/keeyzar/tutorial/Magnet.java
@@ -0,0 +1,16 @@
+package de.keeyzar.tutorial;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Magnet extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+}
---
+++ desktop/src/de/keeyzar/tutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.keeyzar.tutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.keeyzar.tutorial.Magnet;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Magnet(), config);
+	}
+}
